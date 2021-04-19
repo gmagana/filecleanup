154380525b61eccbdc6ac2c898c46f5438e2ff80
Gabriel Magana <gmagana@gobae.com>
2021-04-19 21:06:18 +1200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # filecleanup
 A utility written in Go to delete files. Intended to delete the oldest X logs or backup files.
+
+---
+#What filecleanup does
+
+A familiar thing to do on a computer system is to create backups from scripts. These backups must then be erased or cleaned up manually, usually by deleting the oldest backups and keeping the mst recent X backups.
+
+*filecleanup* is a cross-platform utility that obtains a list of these backups, sorts them in memory, and then deletes files in order until the last X item remain. In the above-mentioned example of backup files, this has the effect of deleting the oldest files until only the most recent files remain.
+
+This utility can then be used as part of your backup script so that only the most recent automated backups are kept on your disk.
+
+##Configuration Process
+
+File deletion is a dangerous thing. Before you go off deleting files, you should follow a process to make sure you have your commands configured correctly, so they will delete the correct files in unattended mode.
+
+By default, *filecleanup* works in dry run mode, meaning no files are actually deleted. When you are confident in your configuration and are ready to destroy files, use the `-live-run` flag.
+
+###Suggested Workflow
+
+We suggest you follow the following process to configure your *filecleanup* commands:
+
+1. Execute your command using the various flags (see below), including the `-list-all-files` flag so you can see what the full sorted file list looks like.
+1. When you have your command line arguments perfected, use the `-live-run` argument to start deleting files.
+
+---
+
+#Usage
+
+`filecleanup [arguments] <file filter>`
+
+***By default this utility runs in dry run (no files are actually deleted) unless you specify the `-live-run` flag. By default, you cannot shoot yourself in the foot.***
+
+## Arguments
+
+Argument | Comments
+---|---
+`-files-to-keep <int>` | (REQUIRED) The number of files you intend to exist after the deletion takes place
+`-list-all-files` | Show list of all files in order in which they will be processed
+`-live-run` | If specified, files are deleted. If not specified, a dry run takes place (no files are deleted)
+`-order-case-insensitive` | Sorts filenames in case insensitive order
+`-order-reverse` | Sorts filenames in reverse order
+`<file filter>` | File naming pattern of the files that should be processed. In essence you can use `*` (matches any number of characters) and `?` (matches a single character) in your file masks. Full documentation for how to format this argument is [here](https://hackage.haskell.org/package/Glob-0.9.2/docs/System-FilePath-Glob.html#v:compile). 
---
+++ src/filecleanup.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+func main() {
+	fmt.Println("filecleanup v1.0 - https://github.com/gmagana/filecleanup")
+	fmt.Println("© 2021 - Gabriel Magana-Gonzalez")
+	fmt.Println()
+	//fmt.Println(fmt.Sprintf("Args (Count: %d):", len(os.Args[1:])))
+	//fmt.Println(os.Args[1:])
+
+	var liveRun bool
+	var listAllFiles bool
+	var caseInsensitiveSort bool
+	var reverseOrder bool
+	var filesToKeep int
+
+	flag.BoolVar(&liveRun,
+		"live-run",
+		false,
+		"If specified, files are deleted. If not specified, a dry run takes place (no files are deleted)")
+
+	flag.BoolVar(&listAllFiles,
+		"list-all-files",
+		false,
+		"Show list of all files in order in which they will be processed")
+
+	flag.BoolVar(&caseInsensitiveSort,
+		"order-case-insensitive",
+		false,
+		"Sorts filenames in case insensitive order")
+
+	flag.BoolVar(&reverseOrder,
+		"order-reverse",
+		false,
+		"Sorts filenames in reverse order")
+
+	flag.IntVar(&filesToKeep,
+		"files-to-keep",
+		0,
+		"(REQUIRED) The number of files you intend to exist after the deletion takes place")
+
+	flag.Parse()
+
+	AssureRequiredFlagsPresent([]string{"files-to-keep"})
+
+	if len(flag.Args()) != 1 {
+		fmt.Fprintf(os.Stderr, "ERROR: Please specify one file pattern filter (Arguments: %s)\n", flag.Args())
+		os.Exit(2) // the same exit code flag.Parse uses
+	}
+
+	targetPattern := flag.Args()[0]
+
+	fmt.Println("\tOPTIONS:")
+	fmt.Println(fmt.Sprintf("\tDRY RUN: %t", !liveRun))
+	fmt.Println(fmt.Sprintf("\tLIST ALL FILES: %t", listAllFiles))
+	fmt.Println(fmt.Sprintf("\tCASE INSENSITIVE ORDER: %t", caseInsensitiveSort))
+	fmt.Println(fmt.Sprintf("\tREVERSE ORDER: %t", reverseOrder))
+	fmt.Println(fmt.Sprintf("\tTARGET PATTERN: %s", targetPattern))
+	fmt.Println(fmt.Sprintf("\tFILES TO KEEP: %d", filesToKeep))
+
+	files, err := filepath.Glob(targetPattern)
+
+	if err != nil {
+		fmt.Println(fmt.Sprintf("ERROR: %s", err))
+		os.Exit(2)
+	}
+
+	// Sort existing filenames depending on specified flags
+	switch {
+	case caseInsensitiveSort && reverseOrder:
+		sort.Slice(files,
+			func(i, j int) bool {
+				return strings.ToLower(files[i]) > strings.ToLower(files[j])
+			})
+	case !caseInsensitiveSort && reverseOrder:
+		sort.Slice(files,
+			func(i, j int) bool {
+				return files[i] > files[j]
+			})
+	case caseInsensitiveSort && !reverseOrder:
+		sort.Slice(files,
+			func(i, j int) bool {
+				return strings.ToLower(files[i]) < strings.ToLower(files[j])
+			})
+	case !caseInsensitiveSort && !reverseOrder:
+		sort.Slice(files,
+			func(i, j int) bool {
+				return files[i] < files[j]
+			})
+	default:
+		panic("Not implemented")
+	}
+
+	if listAllFiles {
+		fmt.Println()
+		fmt.Println("All files (sorted):")
+		for _, file := range files {
+			fmt.Println(file)
+		}
+		fmt.Println()
+	}
+
+	fileCountToDelete := len(files) - filesToKeep
+	var filesToDelete []string = nil
+	if fileCountToDelete >= 1 {
+		filesToDelete = files[0:fileCountToDelete]
+	} else {
+		fileCountToDelete = 0
+	}
+
+	if !liveRun {
+		fmt.Println("*** DRY RUN: No files will be deleted (run with --live-run parameter to delete files) ***")
+	}
+	fmt.Println(fmt.Sprintf("Files found: %d - Files to delete: %d", len(files), fileCountToDelete))
+	for _, file := range filesToDelete {
+		fmt.Println(fmt.Sprintf("DELETING FILE: %s", file))
+	}
+	if !liveRun {
+		fmt.Println("*** DRY RUN: No files deleted ***")
+	}
+	fmt.Println()
+
+	fmt.Println("Done.")
+}
+
+func AssureRequiredFlagsPresent(required []string) {
+	seen := make(map[string]bool)
+	flag.Visit(func(f *flag.Flag) { seen[f.Name] = true })
+	for _, req := range required {
+		if !seen[req] {
+			fmt.Fprintf(os.Stderr, "ERROR: Missing required --%s argument\n", req)
+			os.Exit(2) // the same exit code flag.Parse uses
+		}
+	}
+}
